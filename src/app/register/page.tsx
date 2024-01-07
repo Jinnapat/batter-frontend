31b4de9917fc601086565a494a301762f647a374
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-08 01:15:45 +0700

    add register function and input validations

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,6 +4,8 @@ import CenterContentCard from "@/components/CenterContentCard";
 import HorizontalLine from "@/components/HorizontalLine";
 import InputWithLabel from "@/components/InputWithLabel";
 import MainButton from "@/components/MainButton";
+import validateEmail from "@/helpers/emailValidator";
+import supabaseClient from "@/supabase/client";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -13,8 +15,44 @@ const RegisterPage = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [repassword, setRepassword] = useState<string>("");
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isDone, setIsDone] = useState<boolean>(false);
 
-  const register = () => {};
+  const locked = isProcessing || isDone;
+  const validFirstName = firstName != "";
+  const validLastName = lastName != "";
+  const validEmail = validateEmail(email);
+  const validPassword = password.length >= 6;
+  const validRepassword = repassword.length >= 6 && password === repassword;
+  const valid =
+    validFirstName &&
+    validLastName &&
+    validEmail &&
+    validPassword &&
+    validRepassword;
+
+  const register = async () => {
+    setIsProcessing(true);
+    setErrorMessage("");
+    try {
+      const signUpResult = await supabaseClient.auth.signUp({
+        email,
+        password,
+        options: {
+          emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/email_confirmed?first_name=${firstName}&last_name=${lastName}&email=${email}`,
+        },
+      });
+      if (signUpResult.error) {
+        setErrorMessage(signUpResult.error.message);
+        return;
+      }
+      setIsDone(true);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
   return (
     <CenterContentCard>
       <div>
@@ -28,33 +66,53 @@ const RegisterPage = () => {
         handler={setFirstName}
         labelText="First Name"
         hintText="enter your first name"
+        error={!validFirstName}
+        disabled={locked}
       />
       <InputWithLabel
         handler={setLastName}
         labelText="Last Name"
         hintText="enter your last name"
+        error={!validLastName}
+        disabled={locked}
       />
       <InputWithLabel
         handler={setEmail}
         labelText="Email"
         hintText="enter your email"
+        error={!validEmail}
+        disabled={locked}
       />
       <InputWithLabel
         handler={setPassword}
         labelText="Password"
         hintText="at least 6 characters"
         obsure={true}
+        error={!validPassword}
+        disabled={locked}
       />
       <InputWithLabel
         handler={setRepassword}
         labelText="Repeat Password"
         hintText="enter your password again"
         obsure={true}
+        error={!validRepassword}
+        disabled={locked}
       />
       <br></br>
-      <MainButton handler={register} loading={false}>
+      <MainButton
+        handler={register}
+        loading={isProcessing}
+        disabled={!valid || locked}
+      >
         REGISTER
       </MainButton>
+      <p className="text-red-700 text-center">{errorMessage}</p>
+      {isDone && (
+        <p className="text-center">
+          Almost done! Please check your email and verify your account
+        </p>
+      )}
       <HorizontalLine />
       <p className="text-center">
         Already have an account? login with this{" "}
