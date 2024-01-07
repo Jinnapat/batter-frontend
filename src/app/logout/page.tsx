8fa5b42206abfff8220a0e03192e9550739a734e
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-08 01:19:51 +0700

    add logout page

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import supabaseClient from "@/supabase/client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const LogoutPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    supabaseClient.auth.signOut().then(
+      () => {
+        router.push("/");
+      },
+      () => {
+        router.push("/profile");
+      }
+    );
+  });
+  return <p>logging out...</p>;
+};
+
+export default LogoutPage;
