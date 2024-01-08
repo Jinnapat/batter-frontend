d73e958a06310e06f933094b626440a663b9f6b3
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-08 17:28:36 +0700

    remove database typing

--- src/supabase/client.tsx
+++ src/supabase/client.tsx
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "./supabase";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
-const supabaseClient = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabaseClient;
