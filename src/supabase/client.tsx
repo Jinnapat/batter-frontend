2e6b384cff5f50652b82d477863f60f70b23389f
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-08 17:04:21 +0700

    add database type to client

--- src/supabase/client.tsx
+++ src/supabase/client.tsx
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./supabase";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabaseClient;
