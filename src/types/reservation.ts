be02c2e9d900f3a71f5b3e08555dd76bd686a67f
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-09 00:24:27 +0700

    add missin fields

--- src/types/reservation.ts
+++ src/types/reservation.ts
@@ -1,8 +1,11 @@
 export type Reservation = {
   id: string;
   name: string;
+  description: string;
   status: string;
   lat: number;
   lng: number;
+  start: string;
+  end: string;
   updated_at: string;
 };
