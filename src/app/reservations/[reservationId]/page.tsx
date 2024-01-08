48edea35714a00d2e515c3a567e51b6a8bd92213
Jinnapat Yana <genesoft.ai@gmail.com>
2024-01-08 23:33:22 +0700

    add reservation info page

---
+++ src/app/reservations/[reservationId]/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import SessionChecker from "@/components/SessionChecker";
+import { useEffect, useState } from "react";
+import supabaseClient from "@/supabase/client";
+import Loading from "@/components/Loading";
+import HorizontalLine from "@/components/HorizontalLine";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import LeafletMapWrapper from "@/components/LeafletMapWrapper";
+import "leaflet/dist/leaflet.css";
+import { Reservation } from "@/types/reservation";
+
+const InfoSlot = ({ title, value }: { title: string; value: string }) => {
+  return (
+    <>
+      <div className="flex flex-row items-center">
+        <p className="w-1/3">{title}</p>
+        <p className="w-2/3 text-wrap">{value}</p>
+      </div>
+      <div className="h-0 w-full border my-3"></div>
+    </>
+  );
+};
+
+const ReservationInfoPage = ({
+  params,
+}: {
+  params: { reservationId: string };
+}) => {
+  const [isGettingReservationInfo, setIsGettingReservationInfo] =
+    useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [reservation, setReservation] = useState<Reservation | null>(null);
+
+  useEffect(() => {
+    supabaseClient
+      .from("reservations")
+      .select()
+      .eq("id", params.reservationId)
+      .then((response) => {
+        if (response.error) {
+          setErrorMessage(response.error.message);
+          return;
+        }
+        setReservation(response.data[0]);
+        setIsGettingReservationInfo(false);
+      });
+  }, [params]);
+
+  return (
+    <SessionChecker jumpToIfUnauthenticated="/login">
+      {isGettingReservationInfo && <Loading />}
+      {!isGettingReservationInfo && reservation && (
+        <div className="flex flex-col gap-3 max-w-2xl w-full">
+          <div className="w-full flex flex-col items-center shadow-lg rounded-2xl p-10 bg-white">
+            <div className="w-full">
+              <Link
+                href="/reservations"
+                className="hover:text-yellow-600 transition-colors duration-300"
+              >
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </Link>
+            </div>
+            <div className="w-full">
+              <h1 className="font-bold text-4xl text-center">
+                Reservation Info
+              </h1>
+              <p className="text-center text-sm mb-3">
+                see information about this reservation
+              </p>
+              <p className="text-sm text-center text-red-600">{errorMessage}</p>
+              <HorizontalLine />
+              <InfoSlot title="Event Name" value={reservation.name} />
+              <InfoSlot title="Description" value={reservation.description} />
+              <InfoSlot
+                title="Start"
+                value={new Date(reservation.start).toLocaleString()}
+              />
+              <InfoSlot
+                title="End"
+                value={new Date(reservation.end).toLocaleString()}
+              />
+              <InfoSlot title="Status" value={reservation.status} />
+              <InfoSlot
+                title="Last Status Update"
+                value={new Date(reservation.updated_at).toLocaleString()}
+              />
+            </div>
+          </div>
+          <div className="shadow-lg rounded-2xl p-5 bg-white w-full flex flex-col gap-3">
+            <p className="text-center">
+              Location: {reservation.lat}, {reservation.lng}
+            </p>
+            <LeafletMapWrapper lat={reservation.lat} lng={reservation.lng} />
+            <p className="text-center text-red-600">{errorMessage}</p>
+          </div>
+        </div>
+      )}
+    </SessionChecker>
+  );
+};
+
+export default ReservationInfoPage;
--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -4,25 +4,21 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import * as L from "leaflet";
 import "leaflet-defaulticon-compatibility";
 
-import {
-  MapContainer,
-  Marker,
-  Popup,
-  TileLayer,
-  useMapEvents,
-} from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
 
 function LocationMarker({
   position,
   setPosition,
 }: {
   position: LatLng | null;
-  setPosition: Dispatch<SetStateAction<LatLng | null>>;
+  setPosition?: Dispatch<SetStateAction<LatLng | null>>;
 }) {
   const map = useMapEvents({
     click(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
+      if (setPosition) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      }
     },
   });
 
@@ -30,15 +26,21 @@ function LocationMarker({
 }
 
 const LeafletMap = ({
+  lat,
+  lng,
   position,
   setPosition,
 }: {
-  position: LatLng | null;
-  setPosition: Dispatch<SetStateAction<LatLng | null>>;
+  lat?: number;
+  lng?: number;
+  position?: LatLng | null;
+  setPosition?: Dispatch<SetStateAction<LatLng | null>>;
 }) => {
+  console.log(position);
+  console.log(setPosition);
   return (
     <MapContainer
-      center={[13.74, 100.53]}
+      center={lat && lng ? [lat, lng] : [13.74, 100.53]}
       zoom={13}
       scrollWheelZoom={true}
       className="w-full h-96"
@@ -47,7 +49,12 @@ const LeafletMap = ({
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LocationMarker position={position} setPosition={setPosition} />
+      <LocationMarker
+        position={
+          position ? position : lat && lng ? new LatLng(lat, lng) : null
+        }
+        setPosition={setPosition}
+      />
     </MapContainer>
   );
 };
